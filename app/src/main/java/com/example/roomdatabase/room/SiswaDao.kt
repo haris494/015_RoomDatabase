1d5152439e7d2fdff70b5fc2138e98d01588f2f6
haris49 <hariis.459@gmail.com>
2025-11-28 12:53:38 +0700

    Menambahkan query pengurutan nama pada SiswaDao

--- app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
@@ -1,6 +1,9 @@
 package com.example.roomdatabase.room
 
 import androidx.room.Dao
+import androidx.room.Query
 
 @Dao
 interface SiswaDao {
+    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
+}
