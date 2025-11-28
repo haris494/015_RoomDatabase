6c921b1deebbbe10ec97842e44366bb0d1831180
haris49 <hariis.459@gmail.com>
2025-11-28 12:59:39 +0700

    Menambahkan kelas DatabaseSiswa sebagai konfigurasi Room

---
+++ app/src/main/java/com/example/roomdatabase/room/DataBaseSiswa.kt
@@ -0,0 +1,7 @@
+package com.example.roomdatabase.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase() {
--- app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
+++
@@ -1,3 +0,0 @@
-package com.example.roomdatabase.room
-
-data class DatabaseSiswa()
