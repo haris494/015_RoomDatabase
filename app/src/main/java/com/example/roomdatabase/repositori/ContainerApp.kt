d1e4eb0b907a4d9f94857a6ddc5fad16472ae8d9
haris49 <hariis.459@gmail.com>
2025-11-28 13:23:35 +0700

    Menambahkan kelas ContainerDataApp sebagai implementasi ContainerApp

--- app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
@@ -1,5 +1,11 @@
 package com.example.roomdatabase.repositori
 
+import android.content.Context
+
 interface ContainerApp {
     val repositoriSiswa : RepositorySiswa
 }
+
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
