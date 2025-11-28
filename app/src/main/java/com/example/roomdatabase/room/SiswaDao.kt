84f9597c175523821ea4d37f519c77da6ff7bc3e
haris49 <hariis.459@gmail.com>
2025-11-28 12:54:39 +0700

    Tambah fungsi getAllSiswa dan query order by nama

--- app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
@@ -2,8 +2,10 @@ package com.example.roomdatabase.room
 
 import androidx.room.Dao
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiswaDao {
     @Query("SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa() : Flow<List<Siswa>>
 }
